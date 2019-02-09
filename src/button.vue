28efd3bb02c90e82f3ba555776c5b548e192b43e
liboyang <huagongyilby@126.com>
2019-02-09 14:09:00 +0800

    add icon position

--- src/button.vue
+++ src/button.vue
@@ -1,15 +1,34 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${buttonClass}`]:true}">
+    <svg class="icon" aria-hidden="true" v-if="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="button-content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {};
+export default {
+  props: {
+    icon:String,
+    buttonClass:{
+      type:String,
+      default:"left"
+    }
+  }
+};
 </script>
 <style lang="scss">
 .g-button {
+  vertical-align: middle;
   font-size: var(--font-size);
   height: var(--button-height);
   background: var(--button-bg);
-  padding: 0 1em;
+  padding: 0 .8em;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   font: inherit;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
@@ -21,6 +40,25 @@ export default {};
   }
   &:focus {
     outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right:.3em;
+  }
+  > .button-content {
+    order: 2;
+    font-size: var(--font-size);
+  }
+  &.icon-right {
+    > .icon {
+      margin-left:.3em;
+      margin-right:0;
+      order: 2;
+    }
+    > .button-content {
+      order: 1;
+      font-size: var(--font-size);
+    }
   }
 }
 </style>
