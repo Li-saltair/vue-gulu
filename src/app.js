e31a689ed01bf03cf193d4a84315b5a731953aca
liboyang <huagongyilby@126.com>
2019-02-17 12:00:54 +0800

    add karma; add test file; compile package.json start this project

--- src/app.js
+++ src/app.js
@@ -16,54 +16,66 @@ new Vue({
 
 //chai 单元测试
 import chai from "chai";
+import spies from "chai-spies";
 const expect = chai.expect;
-{
-  let div = document.createElement("div")
-  document.body.appendChild(div)
-  const constructor = Vue.extend(Button);
-  const button = new constructor({
-    propsData: {
-      icon: "setting"
-    }
-  });
-  button.$mount(div);
-  //icon
-  const useElement = button.$el.querySelector("use");
-  let href = useElement.getAttribute("xlink:href");
-  expect(href).to.eq("#i-setting");
-  button.$el.remove()
-  button.$destroy()
-}
-{
-  //iconPosition
-  let div = document.createElement("div")
-  document.body.appendChild(div)
-  const constructor = Vue.extend(Button);
-  const button = new constructor({
-    propsData: {
-      icon: "setting",
-      iconPosition:"right"
-    }
-  });
-  button.$mount(div);
-  const svg = button.$el.querySelector("svg");
-  let { order } = window.getComputedStyle(svg);
-  expect(order).to.eq("2");
-  //测试成功后会自动删除该元素，如果测试不成功则会留着这个元素
-  button.$el.remove()
-  button.$destroy()
-}
-{
-  const constructor = Vue.extend(Button);
-  const gButton = new constructor({
-    propsData:{
-      icon:"setting"
-    }
+chai.use(spies);
+try {
+  {
+    let div = document.createElement("div");
+    document.body.appendChild(div);
+    const constructor = Vue.extend(Button);
+    const button = new constructor({
+      propsData: {
+        icon: "setting"
+      }
+    });
+    button.$mount(div);
+    //icon
+    const useElement = button.$el.querySelector("use");
+    let href = useElement.getAttribute("xlink:href");
+    expect(href).to.eq("#i-setting");
+    button.$el.remove();
+    button.$destroy();
+  }
+  {
+    //iconPosition
+    let div = document.createElement("div");
+    document.body.appendChild(div);
+    const constructor = Vue.extend(Button);
+    const button = new constructor({
+      propsData: {
+        icon: "setting",
+        iconPosition: "right"
+      }
+    });
+    button.$mount(div);
+    const svg = button.$el.querySelector("svg");
+    let { order } = window.getComputedStyle(svg);
+    expect(order).to.eq("2");
+    //测试成功后会自动删除该元素，如果测试不成功则会留着这个元素
+    button.$el.remove();
+    button.$destroy();
+  }
+  //点击的测试
+  {
+    const constructor = Vue.extend(Button);
+    const gButton = new constructor({
+      propsData: {
+        icon: "setting"
+      }
+    });
+    gButton.$mount();
+    let spy = chai.spy(function() {});
+    gButton.$on("click", spy);
+    let button = gButton.$el;
+    button.click();
+
+    expect(spy).to.have.been.called();
+  }
+} catch (error) {
+  window.errors = [error];
+} finally {
+  window.errors && window.errors.forEach((error)=>{
+    console.error(error.message)
   })
-  gButton.$mount()
-  gButton.$on('click', function(){
-    expect(1).to.eq(1)
-  })
-  let button = gButton.$el
-  button.click()
 }
