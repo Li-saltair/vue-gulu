6b27afd7ea3b183ebf95c6f3d654033fc5baee7b
liboyang <huagongyilby@126.com>
2019-02-25 13:51:16 +0800

    add row col

---
+++ src/col.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="col" :class="[`span && col-${span}`,offset && `offset-${offset}`]"
+  :style="{paddingLeft:gutter/2 + 'px',paddingRight:gutter/2 + 'px'}" >
+    <div style="border:1px solid #f00;background:#fc0; height:100px;">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Gcol",
+  props: {
+    span: [Number, String],
+    offset: [Number, String]
+  },
+  data(){
+    return {
+      gutter:0
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.col {
+  padding:0 10px;
+  width: 50%;
+  height: 100px;
+  $class: col-;
+  @for $n from 1 through 24 {
+    &.#{$class}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+
+$class: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class}#{$n} {
+      margin-left: ($n / 24) * 100%;
+    }
+  }
+}
+</style>
---
+++ src/row.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row" :style='{marginLeft:- gutter/2 + "px", marginRight:- gutter/2 + "px"}'>
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name:"Grow",
+  props:{
+    gutter:[Number,String]
+  },
+  created(){
+
+  },
+  mounted(){
+    this.$children.forEach((vm)=>{
+      vm.gutter = this.gutter
+    })
+  }
+};
+</script>
+<style lang="scss" scoped>
+.row {
+  display: flex;
+}
+</style>
+
