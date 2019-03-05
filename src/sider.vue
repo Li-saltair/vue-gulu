7b4abf00e26501976e9a45fcef81b3dfd7606896
liboyang <huagongyilby@126.com>
2019-03-05 22:01:58 +0800

    layout简单完成

--- src/sider.vue
+++ src/sider.vue
@@ -1,17 +1,38 @@
 <template>
-  <div class="sider">
-        <slot></slot>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="visible = !visible">Close</button>
     </div>
+  </transition>
 </template>
 <script>
-
 export default {
-    name:'G-Sider'
+  name: "G-Sider",
+  data() {
+    return {
+      visible: true
+    };
+  },
+  methods: {
+  }
 };
 </script>
 <style lang="scss">
-    .sider{
-
-    }
+.sider {
+  position: relative;
+  > button {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+  }
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s;
+}
+.slide-enter, .slide-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  margin-left: -200px;
+}
 </style>
 
