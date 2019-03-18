d739447c0de1beca7cf168ccce5804adcb1a6f6c
liboyang <huagongyilby@126.com>
2019-03-18 17:22:35 +0800

    添加popover基础功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="wrapper" @click="xxx">
-    <div class="content" v-if='visible'>
-      <slot></slot>
+  <div class="wrapper" @click.stop="xxx">
+    <div class="content" ref="popoverWrapper" v-if="visible" @click.stop>
+      <slot name="content"></slot>
     </div>
-    <slot name="trigger"></slot>
+    <span ref="popoverButton">
+      <slot></slot>
+    </span>
   </div>
 </template>
 <script>
@@ -14,22 +16,45 @@ export default {
       visible: false
     };
   },
-  methods:{
-      xxx(){
-          this.visible = !this.visible
+  mounted() {},
+  computed: {},
+  methods: {
+    xxx() {
+      this.visible = !this.visible;
+      if (this.visible) {
+        this.$nextTick(() => {
+          document.body.appendChild(this.$refs.popoverWrapper);
+          let {
+            width,
+            height,
+            top,
+            left
+          } = this.$refs.popoverButton.getBoundingClientRect();
+          console.log(this.$refs.popoverWrapper);
+          this.$refs.popoverWrapper.style.top = `${top}px`
+          this.$refs.popoverWrapper.style.left = left + "px";
+          let eventHandler = () => {
+            this.visible = false;
+            //弹出消失之后需要移除监听器
+            document.removeEventListener("click", eventHandler);
+          };
+          document.addEventListener("click", eventHandler);
+        });
+      } else {
+        console.log("vm 隐藏");
       }
+    }
   }
 };
 </script>
 <style lang="scss" scoped>
-.wrapper{
-    display: inline-block;
-    position:relative;
-    .content{
-        position:absolute;
-        border:1px solid #f00;
-        top:0;
-        left:0;
-    }
+.wrapper {
+  display: inline-block;
+  position: relative;
+}
+.content {
+  position: absolute;
+  border: 1px solid #f00;
+  transform: translateY(-100%)
 }
 </style>
