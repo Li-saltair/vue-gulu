bafd2e585f5796c4ab7741142bcde8b230e9814a
liboyang <huagongyilby@126.com>
2019-04-22 14:12:51 +0800

    完成大部分pager样式

--- src/pager.vue
+++ src/pager.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="g-pager">
+    <span class="g-pager-item prev" :class="{disabled:currentPage === 1}">
+      <Icon name="left"></Icon>
+    </span>
     <span
       v-for="n in pages"
       :key="n"
       class="g-pager-item"
       :class="{active:n === currentPage,separator:n === '...'}"
     >{{n}}</span>
+    <span class="g-pager-item next" :class="{disabled:currentPage === totalPage}">
+      <Icon name="right"></Icon>
+    </span>
   </div>
 </template>
 <script>
+import Icon from "./icon";
 export default {
   name: "Gpager",
+  components: {
+    Icon
+  },
   props: {
     totalPage: {
       type: Number,
@@ -36,9 +46,11 @@ export default {
           this.currentPage - 2,
           this.currentPage + 1,
           this.currentPage + 2
-        ].sort((a, b) => {
-          return a - b;
-        })
+        ]
+          .filter(item => item >= 1 && item <= this.totalPage)
+          .sort((a, b) => {
+            return a - b;
+          })
       )
     ].reduce((prev, current, index, array) => {
       prev.push(current);
@@ -56,32 +68,56 @@ export default {
 <style lang="scss" scoped>
 @import "var";
 .g-pager {
+  display: flex;
+  align-items: center;
   &-item {
     display: inline-flex;
     cursor: pointer;
     font-size: 14px;
     border: 1px solid #ccc;
     border-radius: $border-radius;
-    min-width: 20px;
-    height: 24px;
+    min-width: 28px;
+    height: 28px;
     justify-content: center;
     align-items: center;
+    text-align: center;
     margin: 0 4px;
     &.separator {
       border: 0;
       cursor: auto;
-      &:hover{
-          color:#333;
+      &:hover {
+        color: #333;
       }
     }
     &.active,
     &:hover {
       border-color: #597ef7;
       color: #597ef7;
+      .icon {
+        fill: #597ef7;
+      }
+    }
+    &.disabled {
+      cursor: no-drop;
+      border-color: #dedede;
+      background: #eee;
+      &:hover {
+        border-color: #dedede;
+        svg {
+          fill: #aaa;
+          color: #aaa;
+        }
+      }
+      svg {
+        fill: #aaa;
+      }
     }
     &.active {
       cursor: no-drop;
     }
   }
+}
+.icon {
+  fill: #333;
 }
 </style>
