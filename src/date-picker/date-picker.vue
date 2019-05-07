71559d0b04393f62469a467f04c1ab3ce3b00312
liboyang <huagongyilby@126.com>
2019-05-07 14:04:11 +0800

    测试分支

---
+++ src/date-picker/date-picker.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class='g-date-picker'>
+        
+    </div>
+</template>
+<script>
+import Ginput from './input'
+export default {
+    name:'GdatePicker',
+    components:{
+        Ginput
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .g-date-picker{
+        display:flex;
+    }
+</style>
