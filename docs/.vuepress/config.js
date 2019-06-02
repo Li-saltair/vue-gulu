6e4584bca90c7db6344fdc9a360730d6d097b5dc
liboyang <huagongyilby@126.com>
2019-06-02 14:34:39 +0800

    调整文档目录结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,22 +7,41 @@ module.exports = {
           '/install/',
           '/get-started/',
           {
+            title: '布局',   // 必要的
+            path: '/basic/',      // 可选的, 应该是一个绝对路径
+            collapsable: true, // 可选的, 默认值是 true,
+            sidebarDepth: 2,    // 可选的, 默认值是 1
+            children: [
+                '/basic/layout',
+                '/basic/grid',
+            ]
+          },
+          {
+            title: '通用',   // 必要的
+            path: '/common/',      // 可选的, 应该是一个绝对路径
+            collapsable: true, // 可选的, 默认值是 true,
+            sidebarDepth: 2,    // 可选的, 默认值是 1
+            children: [
+                '/common/button',
+                '/common/btn-group',
+                '/common/input',
+            ]
+          },
+          {
             title: '简单组件',   // 必要的
             path: '/components/',      // 可选的, 应该是一个绝对路径
             collapsable: true, // 可选的, 默认值是 true,
             sidebarDepth: 2,    // 可选的, 默认值是 1
             children: [
-                '/components/button',
-                '/components/btn-group',
-                '/components/input',
+                // '/components/button',
+                // '/components/btn-group',
+                // '/components/input',
                 //'/components/toast',
                 '/components/popover',
-                '/components/layout',
                 '/components/sticky',
                 '/components/tabs',
                 '/components/collapse',
                 '/components/carousel',
-                '/components/grid',
             ]
           },
         ]
